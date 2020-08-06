3cebd09138f20ad41767841449b4b7cc847f0260
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-08-06 22:27:15 +1000

    Working on the solution for Part2 for the project

--- python-project-starter/part2/part2.py
+++ python-project-starter/part2/part2.py
@@ -1,5 +1,6 @@
 import json
 import plotly.express as px
+from datetime import datetime
 
 def convert_date(iso_string):
     """Converts and ISO formatted date into a human readable format.   
@@ -33,6 +34,8 @@ def process_weather(forecast_file):
 
     min_temps = []
     max_temps = []
+    min_realFeel_temps = []
+    min_realFeelShade_temps = []
     dates = []
     graph_data = {}
 
@@ -42,25 +45,57 @@ def process_weather(forecast_file):
     for day in data["DailyForecasts"]:
         min_temp = convert_f_to_c(day["Temperature"]["Minimum"]["Value"])
         min_temps.append(min_temp)
+        
         max_temp = convert_f_to_c(day["Temperature"]["Maximum"]["Value"])
         max_temps.append(max_temp)
+
+        min_realFeel_temp = convert_f_to_c(day["RealFeelTemperature"]["Minimum"]["Value"])
+        min_realFeel_temps.append(min_realFeel_temp)
+        
+        min_realFeelShade_temp = convert_f_to_c(day["RealFeelTemperatureShade"]["Maximum"]["Value"])
+        min_realFeelShade_temps.append(min_realFeelShade_temp)
+
         date = convert_date(day['Date'])
         dates.append(date)
 
-    df = {
-    "our_data": [123, 567, 435, 345, 678, 900, 345, 233],
-    "more_data": [23, 45, 12, 34, 45, 56, 34, 56],
-    "columns": ["a", "b", "c", "d", "e", "f", "g", "h"]
-    }    
+    print(min_temps)
+    print(max_temps)
+    print(min_realFeel_temps)
+    print(min_realFeelShade_temps)
+    print(dates)
 
-    graph_data = {
-        "mins" : min_temps,
-        "maxs" : max_temps,
-        "dates": dates,
+    # df = {
+    # "our_data": [123, 567, 435, 345, 678, 900, 345, 233],
+    # "more_data": [23, 45, 12, 34, 45, 56, 34, 56],
+    # "columns": ["a", "b", "c", "d", "e", "f", "g", "h"]
+    # }    
+    
+    graph_1_data = {
+    "mins" : min_temps,
+    "maxs" : max_temps,
+    "dates": dates,
+    }
+    
+    graph_2_data = {
+    "mins" : min_temps,
+    "min_realFeel_temps" : min_realFeel_temps,
+    "min_realFeelShade_temps": min_realFeelShade_temps,
+    "dates": dates
     }
 
-    print(graph_data)
+    fig_1 = px.line(
+        graph_1_data, 
+        y = ["mins", "maxs"],
+        x = "dates"
+    )
 
+    fig_2 = px.line(
+        graph_2_data, 
+        y = ["mins", "min_realFeel_temps", "min_realFeelShade_temps"],
+        x = "dates"
+    )
+
+    return fig_1.show(), fig_2.show()
 
 if __name__ == "__main__":
     print(process_weather("data/forecast_5days_a.json"))
